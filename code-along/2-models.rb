541ef6be6bcfffd216083af49ca1f79feb5a5a68
Ilesh Jain <ilesh.jain@kellogg.northwestern.edu>
2024-04-30 03:30:31 +0000

    second sync

--- code-along/2-models.rb
+++ code-along/2-models.rb
@@ -14,14 +14,58 @@ Company.destroy_all
 # 1a. check out the schema file
 # 1b. check out the model file
 
+#puts "There are #{Company.all.count} companies"
+
 # 2. insert new rows in companies table
+
+new_company = Company.new
+new_company["name"] = "Apple"
+new_company["city"] = "Cupertino"
+new_company["state"] = "CA"
+new_company["url"] = "https://apple.com"
+new_company.save
+#puts "There are #{Company.all.count} companies"
+
+new_company = Company.new
+new_company["name"] = "Amazon"
+new_company["city"] = "Seattle"
+new_company["state"] = "WA"
+new_company.save
+#puts "There are #{Company.all.count} companies"
+
+new_company = Company.new
+new_company["name"] = "Twitter"
+new_company["city"] = "San Francisco"
+new_company["state"] = "CA"
+new_company.save
+puts "There are #{Company.all.count} companies"
 
 # 3. query companies table to find all row with California company
 
+all_companies = Company.all 
+cali_companies = Company.where({"state" => "CA"})
+puts "California companies: #{cali_companies.count}"
+
 # 4. query companies table to find single row for Apple
+apple = Company.find_by({"name" => "Apple"})
+p apple
 
 # 5. read a row's column value
 
+p apple["name"]
+p apple["url"]
+p apple["id"]
+
 # 6. update a row's column value
 
+twitter = Company.find_by({"name" => "Twitter"})
+#p twitter
+twitter["url"] = "https://twitter.com"
+twitter["name"] = "X (formerly Twitter)"
+#p twitter
+twitter.save 
+
 # 7. delete a row
+
+x = Company.find_by({"name" => "X (formerly Twitter)"})
+x.destroy 
--- code-along/3-associations.rb
+++ code-along/3-associations.rb
@@ -10,8 +10,47 @@ Contact.destroy_all
 
 # - Insert and read contact data for companies in the database
 
+puts "There are #{Company.all.count} companies"
+
 # 1. insert new rows in the contacts table with relationship to a company
+apple = Company.find_by({"name" => "Apple"})
+amazon = Company.find_by({"name" => "Amazon"})
+
+
+contact = Contact.new
+contact["first_name"] = "Tim"
+contact["last_name"] = "Cook"
+contact["email"] = "tim@apple.com"
+contact["company_id"] = apple["id"]
+contact.save
+
+contact = Contact.new
+contact["first_name"] = "Bob"
+contact["last_name"] = "Smith"
+contact["email"] = "bob@apple.com"
+contact["company_id"] = apple["id"]
+contact.save
+
+contact = Contact.new
+contact["first_name"] = "Jeff"
+contact["last_name"] = "Bezos"
+contact["email"] = "jeff@amazon.com"
+contact["company_id"] = amazon["id"]
+contact.save
+
+puts "There are #{Contact.all.count} contacts"
+
+
 
 # 2. How many contacts work at Apple?
 
+apple_employees = Contact.where({"company_id" => apple["id"]})
+puts "Apple Employees: #{apple_employees.count}"
+
 # 3. What is the full name of each contact who works at Apple?
+
+for employee in apple_employees
+    first_name = employee["first_name"]
+    last_name = employee["last_name"]
+    puts "#{first_name} #{last_name}"
+end
